9fc0939092220fdc0b09eeb05ab43b9ea139b3e8
Kerwick <sullivm@geneseo.edu>
2021-07-30 12:04:09 -0400

    Update GoogleCombined.lua

--- GoogleCombined/GoogleCombined.lua
+++ GoogleCombined/GoogleCombined.lua
@@ -1,4 +1,4 @@
--- About GoogleCombined.lua (version 2.7, 4/7/2021)
+-- About GoogleCombined.lua (version 2.8, 7/30/2021)
 -- Author:  Mark Sullivan, SUNY Geneseo, IDS Project, sullivm@geneseo.edu
 -- GoogleCombined.lua does a search of Google, Google Books and Google Scholar.  Current URL is in the textbox at the top of the Addon for easy cut & paste.
 -- 
@@ -9,57 +9,50 @@
 luanet.load_assembly("System");
 Types = {};
 Types["Process"] = luanet.import_type("System.Diagnostics.Process");
+
 local settings = {};
 settings.AutoSearch = GetSetting("AutoSearch");
-settings.WhichGoogle = GetSetting("WhichGoogle");
-settings.Article = GetSetting("Article");
-
+settings.SearchText = '';
 local interfaceMngr = nil;
-local SearchForm = {};
-
-SearchForm.Form = nil;
-SearchForm.Browser = nil;
-SearchForm.RibbonPage = nil;
-SearchForm.Google= nil;
-SearchForm.GoogleScholar= nil;
-SearchForm.GoogleBooks= nil;
-SearchForm.URLBox = nil;
+local googleSearchForm = {};
+googleSearchForm.Form = nil;
+googleSearchForm.Browser = nil;
+googleSearchForm.RibbonPage = nil;
 
 require "Atlas.AtlasHelpers";
 
-
 function Init()
-        interfaceMngr = GetInterfaceManager();
- 
-        -- Create a form
-        SearchForm.Form = interfaceMngr:CreateForm("Google Search", "Script");
-        -- Add a browser
-        SearchForm.Browser = SearchForm.Form:CreateBrowser("Google Search", "Google Search Browser", "Google Search", "Chromium");
-        -- Hide the text label
-        SearchForm.Browser.TextVisible = false;
+	if GetFieldValue("Transaction", "RequestType") == "Article" then
+		interfaceMngr = GetInterfaceManager();
 		
- 
-        -- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  
-		-- We can retrieve that one and add our buttons to it.
-        SearchForm.RibbonPage = SearchForm.Form:GetRibbonPage("Google Search");
-
-        -- Create the buttons
-        SearchForm.Google= SearchForm.RibbonPage:CreateButton("Search Google", GetClientImage("Search32"), "GoogleSearch", "Google Combined Search");
-        SearchForm.GoogleScholar= SearchForm.RibbonPage:CreateButton("Search Google Scholar", GetClientImage("Search32"), "GoogleScholarSearch", "Google Combined Search");
-        SearchForm.GoogleBooks= SearchForm.RibbonPage:CreateButton("Search Google Books", GetClientImage("Search32"), "GoogleBookSearch", "Google Combined Search");
-	SearchForm.RibbonPage:CreateButton("Open New Browser", GetClientImage("Web32"), "OpenInDefaultBrowser", "Utility");
-
-        -- Hide buttons for Loan/Article
-        if GetFieldValue("Transaction", "RequestType") == "Loan" then
-            -- SearchForm.GoogleScholar.BarButton.Enabled = false;
+		-- Create a form
+		googleSearchForm.Form = interfaceMngr:CreateForm("Google Combined Search", "Script");
+				-- Add a text field for the URL
+		googleSearchForm.URLBox= googleSearchForm.Form:CreateTextEdit("URL:", "URL");
+		
+		-- Add a browser
+		googleSearchForm.Browser = googleSearchForm.Form:CreateBrowser("Google Scholar Search", "Google Scholar Search Browser", "Google Scholar Search");
+		
+		-- Hide the text label
+		googleSearchForm.Browser.TextVisible = false;
+		
+		-- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  We can retrieve that one and add our buttons to it.
+		googleSearchForm.RibbonPage = googleSearchForm.Form:GetRibbonPage("Google Scholar Search");
+		
+		-- Create the search button
+		googleSearchForm.Google= googleSearchForm.RibbonPage:CreateButton("Search Google", GetClientImage("Search32"), "GoogleSearch", "Google Combined Search");
+        googleSearchForm.GoogleScholar= googleSearchForm.RibbonPage:CreateButton("Search Google Scholar", GetClientImage("Search32"), "GoogleScholarSearch", "Google Combined Search");
+        googleSearchForm.GoogleBooks= googleSearchForm.RibbonPage:CreateButton("Search Google Books", GetClientImage("Search32"), "GoogleBookSearch", "Google Combined Search");
+		googleSearchForm.RibbonPage:CreateButton("Open New Browser", GetClientImage("Web32"), "OpenInDefaultBrowser", "Utility");
+		
+		-- After we add all of our buttons and form elements, we can show the form.
+		googleSearchForm.Form:Show();
+		-- Determine Search
+		if GetFieldValue("Transaction", "RequestType") == "Loan" then
             settings.SearchText = GetFieldValue("Transaction", "LoanTitle");
         else
-            -- SearchForm.GoogleBooks.BarButton.Enabled = false;
             settings.SearchText = GetFieldValue("Transaction", "PhotoArticleTitle");
         end
-        -- After we add all of our buttons and form elements, we can show the form.
-        SearchForm.Form:Show();
-
         -- Search when opened if autoSearch is true
 		if settings.AutoSearch then
 			if settings.Article == false then
@@ -80,24 +73,26 @@ function Init()
 				end
 			end
 		end
-		
+	end
 end
 
-
 function GoogleScholarSearch()
-	SearchForm.Browser:Navigate("http://scholar.google.com/scholar?q=" .. AtlasHelpers.UrlEncode(settings.SearchText));
+	googleSearchForm.Browser:Navigate("http://scholar.google.com/scholar?q=" .. AtlasHelpers.UrlEncode(settings.SearchText));	
+	googleSearchForm.URLBox.Value="http://scholar.google.com/scholar?q=" .. AtlasHelpers.UrlEncode(settings.SearchText);
 end
 
 function GoogleBookSearch()
-	SearchForm.Browser:Navigate("http://books.google.com/books?q=" .. AtlasHelpers.UrlEncode(settings.SearchText));
+	googleSearchForm.Browser:Navigate("http://books.google.com/books?q=" .. AtlasHelpers.UrlEncode(settings.SearchText));	
+	googleSearchForm.URLBox.Value="http://books.google.com/books?q=" .. AtlasHelpers.UrlEncode(settings.SearchText);
 end
 
 function GoogleSearch()
-	SearchForm.Browser:Navigate("http://google.com/search?q=" .. AtlasHelpers.UrlEncode(settings.SearchText));
+	googleSearchForm.Browser:Navigate("http://scholar.google.com/scholar?q=" .. AtlasHelpers.UrlEncode(settings.SearchText));	
+	googleSearchForm.URLBox.Value="http://google.com/search?q=" .. AtlasHelpers.UrlEncode(settings.SearchText);
 end
 
 function OpenInDefaultBrowser()
-	local currentUrl = SearchForm.Browser.Address;
+	local currentUrl = googleSearchForm.Browser.Address;
 	
 	if (currentUrl and currentUrl ~= "")then
 		LogDebug("Opening Browser URL in default browser: " .. currentUrl);
